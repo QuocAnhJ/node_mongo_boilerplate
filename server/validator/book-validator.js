5991d3059d17550ac9651d85c52f804a28ed2d60
Johan <johan.tran.0107@gmail.com>
2018-03-07 17:56:53 +0700

    Fix styling error

--- server/validator/book-validator.js
+++ server/validator/book-validator.js
@@ -3,17 +3,17 @@ import {
 } from '../helpers/http-response-helper';
 
 module.exports = {
-  validateRequestBody: async (req, res, next) => {
-      req.checkBody('title', 'Title cannot be blank').notEmpty().trim();
-      req.checkBody('author', 'Author cannot be blank').notEmpty().trim();
-      req.checkBody('genre').trim();
-      req.checkBody('read', 'Read is must boolean type').isBoolean().trim();
+    validateRequestBody: async (req, res, next) => {
+        req.checkBody('title', 'Title cannot be blank').notEmpty().trim();
+        req.checkBody('author', 'Author cannot be blank').notEmpty().trim();
+        req.checkBody('genre').trim();
+        req.checkBody('read', 'Read is must boolean type').isBoolean().trim();
 
-      const errors = req.validationErrors();
+        const errors = req.validationErrors();
 
-      if (errors) {
-          return buildValidationErrorResponse(res, errors);
-      }
-      next();
-  }
+        if (errors) {
+            return buildValidationErrorResponse(res, errors);
+        }
+        next();
+    }
 };
